164dece341af6fa9519830a742dca65610254cee
BrianCheng-TheLegend <briancheng1229@gmail.com>
2024-05-01 10:51:23 +0800

    Rewrite general logistic variable

--- IOT_hw3/logistic.py
+++ IOT_hw3/logistic.py
@@ -1,95 +1,60 @@
 # use logistic regression to approximate OR function
 import numpy as np
 
+# OR function
 x1 = [0, 1, 0, 1]
 x2 = [0, 0, 1, 1]
 y = [0, 1, 1, 1]
 
+# initial variables
 w1 = 2
 w2 = 2
 b =-2
 learning_rate = 0.5
 
+# initial logistic function
 z1 = [0]*4
-y1_old = [0]*4
-y1_new = [0]*4
-y1_minus = [0]*4
-sigma_y1_minus_x1 = 0
-sigma_y1_minus_x2 = 0
-sigma_y1_minus = 0
-w11_new = 0
-w21_new = 0
-b1_new = 0
+y_hat = [0]*4
+y_minus = [0]*4
 
-for i in range(4):
-    z1[i] = w1 * x1[i] + w2 * x2[i] + b
-    y1_old[i] = 1/(1+np.exp(-z1[i]))
-    y1_minus[i]  = y1_old[i]  - y[i] 
-    sigma_y1_minus_x1 += y1_minus[i]*x1[i]
-    sigma_y1_minus_x2 += y1_minus[i]*x2[i]
-    sigma_y1_minus += y1_minus[i]
+# iterations
+iterations = 2
 
-print("y1 = ", y1_old)
-print("y1_minus = ", y1_minus)
-print("sigma_y1_minus_x1 = ", sigma_y1_minus_x1)
-print("sigma_y1_minus_x2 = ", sigma_y1_minus_x2)
-print("sigma_y1_minus = ", sigma_y1_minus)
-
-w11_new = w1 - learning_rate * sigma_y1_minus_x1
-w21_new = w2 - learning_rate * sigma_y1_minus_x2
-b1_new = b - learning_rate * sigma_y1_minus
-print("w11_new = ", w11_new)
-print("w21_new = ", w21_new)
-print("b1_new = ", b1_new)
-
-# variable after 1 iteration
-w1 = w11_new; w2 = w21_new; b = b1_new
-z1 = [0]*4
-y1_old = [0]*4
-
-for i in range(4):
-    z1[i] = w1 * x1[i] + w2 * x2[i] + b
-    y1_old[i] = 1/(1+np.exp(-z1[i]))
+for iteration in range(iterations):
+    for i in range(4):
+        z1[i] = w1 * x1[i] + w2 * x2[i] + b
+        y_hat[i] = 1/(1+np.exp(-z1[i]))
+        y_minus[i] = y_hat[i]  - y[i] 
     
+    # update w1 w2 b
+    w1 = w1 -learning_rate * sum(np.multiply(y_minus,x1))
+    w2 = w2 -learning_rate * sum(np.multiply(y_minus,x2))
+    b = b -learning_rate * sum(y_minus)
 
-print("y1 = ", y1_old)
+    # print iterations, last y hat, updated w1
+    print("Iteration : ",(iteration+1))
+    print("Previous iteration y hat = ", y_hat)
+    print("Update w1 = ", w1)
+    print("Update w2 = ", w2)
+    print("Update b = ", b)
 
-# 2 iteration
-y1_new = [0]*4
-y1_minus = [0]*4
-sigma_y1_minus_x1 = 0
-sigma_y1_minus_x2 = 0
-sigma_y1_minus = 0
-w11_new = 0
-w21_new = 0
-b1_new = 0
+    for i in range(4):
+        z1[i] = w1 * x1[i] + w2 * x2[i] + b
+        y_hat[i] = 1/(1+np.exp(-z1[i]))
+    print("Update y hat = ", y_hat)
 
-for i in range(4):
-    y1_minus[i]  = y1_old[i]  - y[i] 
-    sigma_y1_minus_x1 += y1_minus[i]*x1[i]
-    sigma_y1_minus_x2 += y1_minus[i]*x2[i]
-    sigma_y1_minus += y1_minus[i]
 
-print("y1 = ", y1_old)
-print("y1_minus = ", y1_minus)
-print("sigma_y1_minus_x1 = ", sigma_y1_minus_x1)
-print("sigma_y1_minus_x2 = ", sigma_y1_minus_x2)
-print("sigma_y1_minus = ", sigma_y1_minus)
-
-w11_new = w1 - learning_rate * sigma_y1_minus_x1
-w21_new = w2 - learning_rate * sigma_y1_minus_x2
-b1_new = b - learning_rate * sigma_y1_minus
-print("w11_new = ", w11_new)
-print("w21_new = ", w21_new)
-print("b1_new = ", b1_new)
-
-# variable after 2 iteration
-w1 = w11_new; w2 = w21_new; b = b1_new
-z1 = [0]*4
-y1_old = [0]*4
-
-for i in range(4):
-    z1[i] = w1 * x1[i] + w2 * x2[i] + b
-    y1_old[i] = 1/(1+np.exp(-z1[i]))
-
-print("y1 = ", y1_old)
+### expect result
+# Iteration :  1
+# Previous iteration y hat =  [0.11920292202211755, 0.5, 0.5, 0.8807970779778823]
+# Update w1 =  2.309601461011059
+# Update w2 =  2.309601461011059
+# Update b =  -1.5
+# Update y hat =  [0.18242552380635635, 0.6920245715574536, 0.6920245715574536, 0.9576779336458453]
+# Iteration :  2
+# Previous iteration y hat =  [0.18242552380635635, 0.6920245715574536, 0.6920245715574536, 0.9576779336458453]
+# Update w1 =  2.4847502084094093
+# Update w2 =  2.4847502084094093
+# Update b =  -1.2620763002835544
+# Update y hat =  [0.2206166757677421, 0.7725337652398471, 0.7725337652398471, 0.9760471625441138]
+###
